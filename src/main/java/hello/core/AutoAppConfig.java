95efee3fa661bffd3b02436f991a06f53f37dc50
parksil0 <parksil00@naver.com>
2021-03-23 23:45:52 +0900

    컴포넌트 스캔

---
+++ src/main/java/hello/core/AutoAppConfig.java
@@ -0,0 +1,17 @@
+package hello.core;
+
+import hello.core.member.MemberRepository;
+import hello.core.member.MemoryMemberRepository;
+import org.springframework.context.annotation.*;
+
+@Configuration
+@ComponentScan(
+        excludeFilters = @ComponentScan.Filter(type = FilterType.ANNOTATION, classes = Configuration.class)
+)
+public class AutoAppConfig {
+
+    @Bean(name = "memoryMemberRepository")
+    MemberRepository memberRepository() {
+        return new MemoryMemberRepository();
+    }
+}
